4d909e9c624a87f6fa9321eb0b6960e75cc93ee5
seanmksha <seanmakosharma@gmail.com>
2018-05-07 12:16:20 -0700

    makingsure

--- assets/app/projects/ImageCompressor/ImageCompressor.component.ts
+++ assets/app/projects/ImageCompressor/ImageCompressor.component.ts
@@ -7,7 +7,7 @@ export class ImageCompressorComponent{
     header=`JPEG/JPEG2000 Image Compressor`;
     images=[
         `/images/polyseum/PolyseumSmall.jpg`,
-        `/images/polyseum/Polyseum700x330.jpg`        
+        `/images/imagecompressor/Compression.jpg`        
     ]
 paragraphs=[
  
