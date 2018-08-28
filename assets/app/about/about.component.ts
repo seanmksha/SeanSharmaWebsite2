1cc437b247a697464c2ee5bf1503103ee4744ebb
seanmksha <seanmakosharma@gmail.com>
2018-08-27 21:48:20 -0700

    update

--- assets/app/about/about.component.ts
+++ assets/app/about/about.component.ts
@@ -10,10 +10,10 @@ export class AboutComponent{
     }
  paragraph1=
  `I started with programming Java applications while interning at NASA's Jet Propulsion Lab.  I later moved onto Video Game Programming by leading and participating in multiple team projects for school which included sending video game data to back-end websites. During this time, I began setting up front-end websites on the side as a hobby.  After acquiring my
- Bachelor\'s Degree in Computer Science from the University of Southern California, I focused more on web development and perfecting back-end technologies.  I received his Master's Degree from the University of Southern California, and I have been freelancing for clients since 2017 while constantly learning new technologies.  I also have been assisting and managing part-time in multiple large properties. 
+ Bachelor\'s Degree in Computer Science from the University of Southern California, I focused more on web development and perfecting back-end technologies.  I received my Master's Degree from the University of Southern California, and I have been freelancing for clients since 2017 while constantly learning new technologies.  I also have been assisting and managing part-time in multiple large properties. 
 ` ;
 paragraph2=
 `
-His hobbies include swimming, running, working out, playing with his pets, playing video games, learning new technologies, and of course programming. He resides in California enjoying life as a newly wed.
+My hobbies include swimming, running, working out, playing with his pets, playing video games, learning new technologies, and of course programming.
 `;
 }
