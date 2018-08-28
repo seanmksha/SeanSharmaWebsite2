24addcec11da1de51af7e7989e9ffa5d6ad0d514
seanmksha <seanmakosharma@gmail.com>
2018-08-27 20:52:27 -0700

    desc

--- assets/app/about/about.component.ts
+++ assets/app/about/about.component.ts
@@ -9,8 +9,8 @@ export class AboutComponent{
         
     }
  paragraph1=
- `Sean Sharma started with programming Java applications while interning at NASA's Jet Propulsion Lab.  He later moved onto Video Game Programming by leading and participating in multiple team projects for school which included sending video game data to back-end websites. During this time, he began setting up front-end websites on the side as a hobby.  After acquiring his
- Bachelor\'s Degree in Computer Science from the University of Southern California, he focused more on web development and perfecting back-end technologies.  He received his Master's Degree from the University of Southern California, and he has been freelancing for clients since 2017 while constantly learning new technologies to improve his methods.  
+ `I started with programming Java applications while interning at NASA's Jet Propulsion Lab.  I later moved onto Video Game Programming by leading and participating in multiple team projects for school which included sending video game data to back-end websites. During this time, I began setting up front-end websites on the side as a hobby.  After acquiring my
+ Bachelor\'s Degree in Computer Science from the University of Southern California, I focused more on web development and perfecting back-end technologies.  I received his Master's Degree from the University of Southern California, and I have been freelancing for clients since 2017 while constantly learning new technologies.  I also have been assisting and managing part-time in multiple large properties. 
 ` ;
 paragraph2=
 `
